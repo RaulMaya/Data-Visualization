908cf16e1592cbbbc55de6f130d1df8e193177cd
Raul <raulmayas20@gmail.com>
2022-02-17 21:11:02 -0600

    Molecular Motion

--- python_programs/molecular_motion.py
+++ python_programs/molecular_motion.py
@@ -41,25 +41,24 @@ class RandomWalk:
                         
 
 while True:
-    rw = RandomWalk(50_000)
+    rw = RandomWalk(5_000)
     rw.fill_walk()
 
     # Plotting points in the walk
     plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(20,12))
+    ax.set_facecolor('lightskyblue')
     point_num = range(rw.num_points)
-    ax.scatter(rw.x_values, rw.y_values, c=point_num, cmap=plt.cm.cool, edgecolors='none', s=2)
+    ax.plot(rw.x_values, rw.y_values, c='gold', linewidth = 1.5)
     
-    # Start and end point
-    ax.scatter(0,0, c = 'lightgreen', edgecolors = 'black', s=100)
-    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='black', s=100)
     
     # Removing axes
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
     
+    ax.set_title('Pollen Grain on the Surface of a Drop of Water')
     plt.show()
-    fig.savefig('../outputs/random_walks/rw_output4.png')
+    fig.savefig('../outputs/random_walks/rw_output5.png')
     
     continue_running = input('Make another run? (yes/no):')
     if continue_running == 'no':
