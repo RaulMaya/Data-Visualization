c4896654b27b18badbb4ee4800984976fe87b8da
Raul <raulmayas20@gmail.com>
2022-02-22 12:38:30 -0600

    2 Dices Update

--- python_programs/the_dice/two_dice_visual.py
+++ python_programs/the_dice/two_dice_visual.py
@@ -6,7 +6,7 @@ from dice import Side
 
 # Creating Two D6 Dices
 die_1 = Side(6)
-die_2 = Side(6)
+die_2 = Side(10)
 
 # Make some rolls, and store the results
 results = []
@@ -17,7 +17,7 @@ for ind_roll in range(51500):
 # Reviewing the results
 frec = []
 max_results = die_1.num_sides + die_2.num_sides
-for value in range(1, max_results + 1):
+for value in range(2, max_results + 1):
     frecuency = results.count(value)
     frec.append(frecuency)
 print(frec)
@@ -30,4 +30,4 @@ x_axis_config = {'title':'Result', 'dtick':1}
 y_axis_config = {'title':'Frecuency of Individual Value'}
 
 my_hist = Layout(title='Output of Rolling Two 6-Sided Dice 51500 Times', xaxis = x_axis_config, yaxis = y_axis_config)
-offline.plot({'data':data, 'layout':my_hist}, filename = '../outputs/dice_simulation/two_six_sided.html')
+offline.plot({'data':data, 'layout':my_hist}, filename = 'two_six_sided.html')
