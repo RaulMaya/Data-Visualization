50a32fd4e70facd0417bf9d70a2e0e62e6545d2a
Raul <raulmayas20@gmail.com>
2022-03-24 10:03:10 -0600

    Bar Chart of Mosts Stars per Project

--- python_programs/working with APIs/api_responses.py
+++ python_programs/working with APIs/api_responses.py
@@ -1,5 +1,8 @@
 import requests
 
+from plotly.graph_objs import Bar
+from plotly import offline
+
 url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
 headers = {'Accept':'application/vnd.github.v3+json'}
 r = requests.get(url, headers=headers)
@@ -11,10 +14,39 @@ print(f"Total Repositories: {response['total_count']}")
 repositories = response['items']
 print(f"Founded Repositories: {len(repositories)}")
 # repository = repositories[0]
+repo_names = []
+stared_repos = []
 for repository in repositories:
+    repo_names.append(repository['name'])
     print(repository['name'])
     print(repository['owner']['login'])
+    stared_repos.append(repository['stargazers_count'])
     print(repository['stargazers_count'])
     print(repository['html_url'])
     print(repository['description'])
     print('')
+
+data = [{
+    'type':'bar',
+    'x':repo_names,
+    'y':stared_repos,
+    'marker':{
+        'color': 'rgb(255, 159, 69)',
+        'line':{'width': 3, 'color' : 'rgb(247, 110, 17)'}
+    }
+}]
+
+my_layout = {
+    'title':'Most Stared Python Projects on GitHub',
+    'titlefont':{'size':24},
+    'xaxis': {'title':'Repository Name',
+    'titlefont': {'size':20},
+    'tickfont':{'size':12},
+    },
+    'yaxis': {'title': 'Number of Stars',
+    'titlefont': {'size':20},
+    'tickfont':{'size':12},
+    }}
+
+fig = {'data':data, 'layout':my_layout}
+offline.plot(fig, filename='python_repos.html')
