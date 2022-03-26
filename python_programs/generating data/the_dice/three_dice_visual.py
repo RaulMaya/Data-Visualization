be73b26e9d3d4053c529bd6bfa6abf6e61ae6c0f
Raul <raulmayas20@gmail.com>
2022-03-25 22:42:23 -0600

    Plotly Update

--- python_programs/generating data/the_dice/three_dice_visual.py
+++ python_programs/generating data/the_dice/three_dice_visual.py
@@ -24,10 +24,10 @@ print(frec)
 
 # Feeding the histogram
 x_values = list(range(3, max_results+1))
-data = [Bar(x=x_values, y=frec)]
+data = [Bar(x=x_values,y=frec, marker=dict(color='rgba(192, 237, 166, 0.6)',line=dict(color='rgba(255, 247, 188, 1.0)', width=3)))]
 
 x_axis_config = {'title':'Result', 'dtick':1}
 y_axis_config = {'title':'Frecuency of Individual Value'}
 
-my_hist = Layout(title='Output of Rolling Three 6-Sided Dice 51500 Times', xaxis = x_axis_config, yaxis = y_axis_config, colorway=['#E54066'])
+my_hist = Layout(title='Output of Rolling Three 6-Sided Dice 51500 Times',title_x=0.5, xaxis = x_axis_config, yaxis = y_axis_config)
 offline.plot({'data':data, 'layout':my_hist}, filename = 'three_six_sided.html')
